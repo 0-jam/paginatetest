44a910b9d4faadc0eb73e78d12706d9a56980b45
0-jam <28038981+0-jam@users.noreply.github.com>
2020-10-23 11:38:39 +0900

    Product page as root

--- config/routes.rb
+++ config/routes.rb
@@ -32,5 +32,6 @@
 
 Rails.application.routes.draw do
   resources :product
+  root to: 'product#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
